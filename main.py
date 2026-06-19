68e9e0766147e5040a31c7b840e3dae54131025a
khadijafsd2011-svg <khadijafsd2011@gmail.com>
2026-06-19 22:47:06 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hello pyton!")
+print(6)
+print("hello python!" , 5 ,7)
+
+a=6
+b=5
+print(a,b)
+
+print("hello \n world! ")
+print("how are you?")
---
+++ main2.py
@@ -0,0 +1,4 @@
+#print all the keywords in python
+print("keywords in python are ......")
+import keyword
+print(keyword.kwlist)
